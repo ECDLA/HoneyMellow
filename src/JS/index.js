9fa33f4be898d47de4e8ade012cffa905e0d1005
Naoko <josefloresgonzalez@proton.me>
2026-05-01 22:15:43 -0600

    Feat (Sistema): Se escaneó y extrajo toda la información de la carpeta de Drive con las traducciones de Yami

--- src/JS/index.js
+++ src/JS/index.js
@@ -1,11 +1,17 @@
-// const urlAPI = "https://mellow-api.azurewebsites.net/api/FunctionMellowAPI"
-const urlAPI = "db.json"
-const cuerpovideos = document.querySelector("[data-videos]");
-let cacheDeVideos = null;
+// ----- Configuración global --------------------------------------------------------------------------------
+const urlMellowAPI = "videosYami.json" // Usar la URL cuando se quiera conectar a Cosmos DB: https://mellow-api.azurewebsites.net/api/FunctionMellowAPI
+const cuerpoVideosDOM = document.querySelector("[data-videos]");
 
+// ----- CONF. renderizarMasVideos() --------------------------------------------------------------------------------
+let listaVideos = [];
+let videosPorPagina = 20;
+let limiteVideos = videosPorPagina;
+let desplazamiento = 0;
+
+// ----- CONEXIÓN A LA BD --------------------------------------------------------------------------------
 async function conexionAPI() {
     try {
-        let conexion = await fetch(urlAPI);
+        let conexion = await fetch(urlMellowAPI);
         let conexionConvertidaJSON = await conexion.json()
 
         // Quitar .videos cuando se use la API
@@ -16,80 +22,111 @@ async function conexionAPI() {
     }
 }
 
-function crearFicha(id, tituloRomaji, tituloEspañol, urlDrive, urlYoutube) {
-    let ficha = document.createElement("div");
-    let idYoutube = obtenerIdVideo(urlYoutube);
-
-    ficha.className = "video";
-    ficha.innerHTML =
-        `
-        <a href="https://drive.google.com/file/d/${obtenerIdVideo(urlDrive)}/preview" target="_blank">
-            <figure class="imagen">
-                <img class="miniatura" class="miniatura" src="https://img.youtube.com/vi/${idYoutube}/mqdefault.jpg" alt="" id="${id}">
-                
-            </figure>
-        </a>
-
-        <p>${id} - ${tituloRomaji} ${tituloEspañol}</p>
-    `
-
-    return ficha;
-}
-
-async function obtenerYMostrarVideos() {
-    // Hace una llamada a la API entregando un array de objetos
-    let listaVideos = await conexionAPI();
-
-    // Ordena la lista mediante la función .sort()
-    listaVideos.sort(
-        // .sort () envia dos númeos a comprar a la función anonima
-        function comparar(a, b) {
-            // Compara cuanto es la resta y si es negativo entonces a va delante de b [b, a]
-            return Number(b.id) - Number(a.id);
-        }
-    );
-    console.log(listaVideos)
-    listaVideos.forEach(video => {
-        cuerpovideos.appendChild(
-            crearFicha(video.id, video.tituloRomaji, video.tituloEspañol, video.urlDrive, video.urlYoutube)
-        );
-    }
-    );
-}
-
-obtenerIdVideo("https://drive.google.com/file/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs/view?usp=sharing")
-
-function obtenerIdVideo(url) {
+// ----- EXTRACIÓN DE ID'S --------------------------------------------------------------------------------
+function obtenerIDVideo(url) {
     // Se crea un objeto usando la clase URL la cual ya conoce la estructura de una url por lo que sabe identificar partes claves como ".hostname".
     const urlObjeto = new URL(url);
-    let idYoutube = "";
+    let idVideo = "";
 
     // Usando la url ya convertida a objeto, ve si la url incluye o viene de "youtu.be".
     if (urlObjeto.hostname.includes("youtu.be")) {
 
         // Con .slice se recorre una posición para eliminar "/"
-        idYoutube = urlObjeto.pathname.slice(1);
+        idVideo = urlObjeto.pathname.slice(1);
 
     } else if (urlObjeto.hostname.includes("youtube.com")) {
 
         // Busca dentro del objeto en "searchParams" el parametro "v" y retorna el valor con .get
-        idYoutube = urlObjeto.searchParams.get("v");
+        idVideo = urlObjeto.searchParams.get("v");
 
     } else if (urlObjeto.hostname.includes("drive.google.com")) {
-        idYoutube = urlObjeto
+        idVideo = urlObjeto
             .pathname
             .slice(8)
             .replace("/view", "")
             .replace("?usp=sharing", "")
             .replace("/preview", "")
-        ;
-                
+            ;
+
     } else {
         console.log("Url de video incorrecta");
         return null;
     }
 
-    return idYoutube;
+    return idVideo;
 }
 
-obtenerYMostrarVideos();
+// ----- CREAR ficha DE VIDEO EN HTML --------------------------------------------------------------------------------
+// Preview Google Drive: https://drive.google.com/file/d/${obtenerIDVideo(urlVideo)}/preview
+// Imagen YouTube: https://img.youtube.com/vi/${idVideo}/mqdefault.jpg
+function crearTarjetaVideo(numeracionDrive, tituloRomaji, tituloEspañol, urlDrive, urlYoutube) {
+    let fichaDOM = document.createElement("div");
+    let idURL = obtenerIDVideo(urlDrive);
+    let urlMiniatura = "";
+    console.log(urlYoutube)
+
+    if (urlYoutube == "") {
+        idURL = obtenerIDVideo(urlDrive);
+        urlMiniatura = `https://drive.google.com/thumbnail?id=${idURL}`
+    } else {
+        idURL = obtenerIDVideo(urlYoutube);
+        urlMiniatura = `https://img.youtube.com/vi/${idURL}/mqdefault.jpg`
+    }
+
+    fichaDOM.innerHTML =
+        `
+        <a href="https://drive.google.com/file/d/${idURL}/preview" target="_blank">
+            <figure class="imagen">
+                <img class="miniatura" class="miniatura" src="${urlMiniatura}" alt="" id="${numeracionDrive}">
+                
+            </figure>
+        </a>
+
+        <p>${numeracionDrive} - ${tituloRomaji} 『${tituloEspañol}』</p>
+    `
+
+    return fichaDOM;
+}
+
+// ----- OBTIENE LA LISTA DE VIDEOS Y LOS ORGANIZA --------------------------------------------------------------------------------
+async function obtenerVideos() {
+    // Hace una llamada a la API entregando un array de objetos
+    listaVideos = await conexionAPI();
+
+    // Ordena la lista mediante la función .sort()
+    listaVideos.sort(
+        // .sort() envia dos númeos a comprar a la función anonima
+        function comparar(a, b) {
+            // Compara cuanto es la resta y si es negativo entonces a va delante de b [b, a]
+            return parseFloat(b.idDrive) - parseFloat(a.idDrive);
+        }
+    );
+
+    renderizarVideos(listaVideos, desplazamiento, limiteVideos);
+}
+
+// ------ MUESTRA LOS VIEOS EN PANTALLA --------------------------------------------------------------------------------
+function renderizarVideos(videos, desplazamiento, limiteVideos) {
+    let listaVideosVisible = videos.slice(desplazamiento, limiteVideos);
+
+    listaVideosVisible.forEach(video => {
+        cuerpoVideosDOM.appendChild(
+            crearTarjetaVideo(video.idDrive, video.tituloRomaji, video.tituloEspañol, video.urlDrive, video.urlYoutube)
+        );
+    }
+    );
+}
+
+// ----- RENDERIZAR MÁS VIDEOS EN PANTALLA --------------------------------------------------------------------------------
+function renderizarMasVideos() {
+    limiteVideos += videosPorPagina;
+    desplazamiento += videosPorPagina;
+    renderizarVideos(listaVideos, desplazamiento, limiteVideos);
+
+    if (limiteVideos >= listaVideos.length) {
+        let btnMostrarMasDOM = document.getElementById("btn-mostrar-mas");
+        btnMostrarMasDOM.style.display = "none";
+    }
+}
+
+obtenerVideos();
--- src/JS/obenetVideosDrive.js
+++ src/JS/obenetVideosDrive.js
@@ -1,16 +1,55 @@
-const apiKEY = ""
-const urlCarpetaDriveVideos = "https://drive.google.com/drive/u/0/folders/1laPxK8i4R2nKbn4BVUGFBViTSyGFWTv4"
+// Información necesaria
+const apiKey = "AIzaSyBPq20euVVhXvkzLX5FbdxbBCLdmJH3v3M"
+let folderId = "1laPxK8i4R2nKbn4BVUGFBViTSyGFWTv4"
+
+// Parametros para obtener la información de la url de Google Drive
+const base = "https://www.googleapis.com/drive/v3/files";
+const carpeta = `?q='${folderId}' in parents`;
+const limite = "&pageSize=200";
+const campos = "&fields=files(id,name,description,mimeType)";
+const key = `&key=${apiKey}`;
+
+const url = `${base}${carpeta}${limite}${campos}${key}`;
 
 async function conexionAPI() {
-    const base    = "https://www.googleapis.com/drive/v3/files";
-    const carpeta = `?q='${folderId}' in parents`;
-    const limite  = "&pageSize=100";
-    const campos  = "&fields=files(id,name,description,mimeType,modifiedTime)";
-    const key     = `&key=${apiKey}`;
-
-
-    let conexion = await fetch("https://www.googleapis.com/drive/v3/files?q='${folderId}'&pageSize=100&fields=files(id,name,description,mimeType,modifiedTime)`&key=${apiKey}`");
+    let conexion = await fetch(url);
     let conexionConvertida = await conexion.json();
 
-    return conexionConvertida;
+    return conexionConvertida.files;
 }
+
+async function transformarDatos() {
+    const videos = await conexionAPI();
+    const contenido = videos.map((video, indice) => {
+        titulo = extraerTitulo(video.name);
+        return {
+            // id: `${indice + 1}`,
+            idDrive: titulo.idDrive,
+            tituloRomaji: titulo.romaji,
+            tituloEspañol: titulo.espanol,
+            urlDrive: `https://drive.google.com/file/d/${video.id}/view`,
+            urlYoutube: ''
+        }
+    })
+
+    // const fs = require('fs');
+    // fs.writeFileSync(
+    //     'videosYami.json',
+    //     JSON.stringify(contenido, null, 2)
+    // );
+    // console.log(` ${contenido.length} documentos exportados`)
+}
+
+function extraerTitulo(titulo) {
+    const idDrive = titulo.split(" ")[0];
+    const romaji = titulo.split("[")[0].split(" ").slice(1).join(" ").trim();
+    let espanol = "";
+
+    if (titulo.includes("[")) {
+        espanol = titulo.split("[")[1].split("]")[0];
+    }
+
+    return { idDrive, romaji, espanol };
+}
+
+transformarDatos()
